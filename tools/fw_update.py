7dae10c9d6ec589951475d501f0b734779d526f8
CrolineCrois <maoninii@gmail.com>
2023-08-02 21:16:20 +0000

    change from size for CBC

--- tools/fw_update.py
+++ tools/fw_update.py
@@ -16,7 +16,7 @@ OK = b"\x00"
 ERROR = b"\x01"
 END = b"\x02"
 
-FRAME_SIZE = 1057
+FRAME_SIZE = 1105
 
 # Sends START frame
 # Takes serial object, meta frame, and debug
@@ -89,11 +89,11 @@ def update(ser, infile, debug):
         firmware_blob = fp.read()
 
     # Send START frame
-    metadata = firmware_blob[:1057]
+    metadata = firmware_blob[:1105]
     send_metadata(ser, metadata, debug=debug)
 
     # Send DATA, MESSAGE, and END frames
-    firmware = firmware_blob[1057:]
+    firmware = firmware_blob[1105:]
     for idx, frame_start in enumerate(range(0, len(firmware), FRAME_SIZE)):
         # Chunk and write frames
         data = firmware[frame_start : frame_start + FRAME_SIZE]
